f506820667918c528abf3a10bba73bfc8aa966bc
MohamedAmineBENALI <100967543+MohamedAmineBENALI@users.noreply.github.com>
2022-08-31 04:44:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app=FastAPI()
 app.include_router(api)
 
 
-origins = ["http://localhost","http://localhost:4200","http://localhost:4201","http://127.0.0.1:8080","http://192.168.1.7:8080","http://172.20.240.1:8080"]
+origins = ["https://a865-197-1-81-117.eu.ngrok.io","http://localhost","http://localhost:4200","http://localhost:4201","http://127.0.0.1:8080","http://192.168.1.7:8080","http://172.20.240.1:8080"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
