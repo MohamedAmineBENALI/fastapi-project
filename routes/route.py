e34822bf9f729e06661b67f5f309d374942600ff
MohamedAmineBENALI <100967543+MohamedAmineBENALI@users.noreply.github.com>
2022-08-31 18:24:21 +0200

    Update route.py

--- routes/route.py
+++ routes/route.py
@@ -9,11 +9,11 @@ api =APIRouter()
     
 
 @api.get("/param/select")
-async def paramSelect():
-    return conn.execute(demo.select()).first()
+def paramSelect():
+    return conn.execute(demo.select()).fetchall()
 
 @api.put("/param/param_update")
-async def param_update(d:sh_demo):
+def param_update(d:sh_demo):
     conn.execute(demo.update().values(
         time=d.time,
         speed=d.speed,
